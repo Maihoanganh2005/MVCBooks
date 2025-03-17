198e2ab74453da4a5115053543b8dec4afa92c82
Maihoanganh2005 <hoanganh05052005@gmail.com>
2025-03-17 08:47:37 +0700

    Add project files.

---
+++ MVCBooks/Controllers/FirstController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCBooks.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string HovaTen, int Tuoi = 1)
+        {
+            ViewData["Greeting"] = "Xin chao, toi la " + HovaTen + ". Tuoi cua toi la " + Tuoi.ToString() + ".";
+            return View();
+        }
+    }   
+}
---
+++ MVCBooks/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCBooks.Data;
+using MVCBooks.Models;
+using System.Globalization;
+
+var cultureInfo = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MVCBooksContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCBooksContext") ?? throw new InvalidOperationException("Connection string 'MVCBooksContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
